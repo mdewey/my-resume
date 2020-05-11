eee635a9adc8f559e45b8d63c358306e349f7e7a
Mark Dewey <mark.t.dewey@gmail.com>
2020-05-11 15:37:01 -0400

    styled education

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -2,14 +2,16 @@ import React from "react"
 import "./style.scss"
 const index = () => {
   return (
-    <div>
-      <h1>Bachelors of Science; Computer Science</h1>
-      <h2>University Pittsburgh at Johnstown</h2>
-      <h2>Aug. 2007 - May 2011</h2>
-      <h2>GPA: 3.7</h2>
-      <h3>Presidential Scholar</h3>
+    <div className="education">
+      <section className="details">
+        <h1>Bachelors of Science; Computer Science</h1>
+        <h2>University Pittsburgh at Johnstown</h2>
+        <h2>Aug. 2007 - May 2011</h2>
+        <h2>GPA: 3.7</h2>
+        <h3>Presidential Scholar</h3>
+      </section>
       <section className="notable-classes">
-        <header>Notable Classes</header>
+        <header>notable classes:</header>
         <ul>
           <li>Data Structures</li>
           <li>Algorithms</li>
