5ac272fd77e3e3530237d987d283a78fe2e4f9f9
Mark Dewey <mark.t.dewey@gmail.com>
2020-05-13 09:50:10 -0400

    hiding what I want to hide

---
+++ src/components/Employment/Job.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { useShowAll } from "../../context/ShowAllContext"
+
+const Job = ({ job }) => {
+  const { showAll } = useShowAll()
+  return (
+    <li className="job">
+      <section className="details">
+        <h2>
+          <a href={job.website} target="_blank" rel="noopener noreferrer">
+            {job.company}
+          </a>
+        </h2>
+        <h3>{job.position}</h3>
+        <h4 className="where">{job.location}</h4>
+        <h4 className="when">
+          {new Date(job.startDate).toLocaleDateString()} -{" "}
+          {job.endDate ? new Date(job.endDate).toLocaleDateString() : "present"}
+        </h4>
+      </section>
+      <section className="summary">
+        <header className="job-description">{job.summary}</header>
+        <section className="projects">
+          <p>projects and responsibilities:</p>
+          <ul>
+            {job.projects.map((project, index) => {
+              if (project.canHide && !showAll) {
+                return <></>
+              }
+              return (
+                <li key={index} className="project">
+                  <header>{project.name}</header>
+                  <section className="technology">
+                    <p>technology used:</p>
+                    <ul className="technologies">
+                      {project.technology.map((tech, index) => {
+                        return <li key={index}>{tech}</li>
+                      })}
+                    </ul>
+                  </section>
+                  <section className="responsibilities">
+                    <p>what I did:</p>
+                    <ul>
+                      {project.responsibilities.map((task, index) => {
+                        return <li key={index}>{task}</li>
+                      })}
+                    </ul>
+                  </section>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+      </section>
+    </li>
+  )
+}
+
+export default Job
