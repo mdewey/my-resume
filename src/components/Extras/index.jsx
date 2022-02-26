77b8a964f8dec200874e34616113ce384fb57989
Mark Dewey <mark.t.dewey@gmail.com>
2022-02-26 10:54:36 -0500

    (content) updated extras

--- src/components/Extras/index.jsx
+++ src/components/Extras/index.jsx
@@ -5,9 +5,10 @@ const index = () => {
     <div className="extras">
       <h1>Extras</h1>
       <ul>
-        <li>Advisory board for PTech</li>
-        <li>Start up weekend winner</li>
-        <li>Completed Music Theory course on Couresa</li>
+        <li>Advisory board for local programs</li>
+        <li>Start up weekend winner and hacker</li>
+        <li>Takes music courses on the side</li>
+        <li>Game master for local gaming groups</li>
         <li>Eagle Scout; Awarded May 2006</li>
       </ul>
     </div>
